dd555b61aec1577f53fb2c7680d6513aa17155a1
Faruq Hossain <faruqdigital01@gmail.com>
2023-03-28 00:37:48 +0600

    count section added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,4 @@
-/**
-* Template Name: Butterfly
-* Updated: Mar 10 2023 with Bootstrap v5.2.3
-* Template URL: https://bootstrapmade.com/butterfly-free-bootstrap-theme/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
